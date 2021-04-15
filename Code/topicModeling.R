475471ad526a96be398c4cbfae83c1b8f76e5b94
Susan Chen <sc6354@nyu.edu>
2021-04-15 15:27:38 -0400

    Update topicModeling.R

--- Code/topicModeling.R
+++ Code/topicModeling.R
@@ -66,3 +66,46 @@ wordcloud2(data.frame(words, prob), shuffle = TRUE, size = .5)
 
 
 
+################
+fatigue_tweets$clean_text <- as.character(fatigue_tweets$clean_text)
+fatigue_corpus <- corpus(fatigue_tweets$clean_text,
+                            docnames = fatigue_tweets$X)
+
+fatigue_tokens <- fatigue_corpus %>%
+      tokens(remove_punct =TRUE, remove_numbers = TRUE, remove_symbols = TRUE) %>%
+      tokens_tolower() %>%
+      tokens_select(pattern = stopwords("en"), selection = "remove") 
+
+fatigue_collocations <- textstat_collocations(fatigue_tokens, min_count =25)
+
+fatigue_corpus_tokens <- tokens_compound(fatigue_tokens, fatigue_collocations)
+
+## Model Calculation 
+DTM2 <- fatigue_corpus_tokens %>%
+      tokens_remove('') %>%
+      dfm() %>%
+      dfm_trim(min_docfreq = .01, max_docfreq =1, docfreq_type = 'prop')  
+
+# the number of documents and terms in the matrix 
+dim(DTM2)
+
+## remove the 10 most common terms 
+textstat_frequency(DTM2)[1:10]
+top10_terms2 <- c('pandemicfatigue', 'covidfatigue', 'pandemic', 'year', 'covid', 'just', 'covid19', 'people', 'one', 'can')
+DTM2 <- DTM2[, !(colnames(DTM2) %in% top10_terms2)]
+
+sel_idx <- rowSums(DTM2) > 0
+DTM2 <- DTM2[sel_idx, ]
+fatigue_tweets <- fatigue_tweets[sel_idx,]
+
+
+topicModel2 <- LDA(DTM2, K, method= 'Gibbs', control = list(iter=500, seed=1, verbose=25))
+
+tmResult2 <- posterior(topicModel2)
+
+attributes(tmResult2)
+
+terms(topicModel2, 10)
+
+top5termsPerTopic2 <- terms(topicModel2, 5)
+topicNames2 <- apply(top5termsPerTopic2, 2, paste, collapse=' ')
